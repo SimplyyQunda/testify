59fe3f52f79a619a0e276a70903472b21584c864
DaQunda Magwood <dmagwood13@gmail.com>
2016-11-03 15:09:33 -0400

    my first test finally past

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
-function fizz () {
-  // TODO
+function fizz (number) {
+	return number;
 };
 
 function fizzBuzz (start, end) {
   // Write fizzBuzz here once fizz is finished.
+
 };
 
 export { fizz };
